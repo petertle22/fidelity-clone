69b3dfc56ea09cf8e8ca96773b45b62aa9edcb4c
petertle22 <petertle@outlook.com>
2024-06-05 22:23:10 -0400

    Added extended trading hours slider

--- src/app/modules/account-and-trade/trade/trade/trade.component.ts
+++ src/app/modules/account-and-trade/trade/trade/trade.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-trade',
   templateUrl: './trade.component.html',
-  styleUrl: './trade.component.scss'
+  styleUrl: './trade.component.scss',
+  standalone: true,
+  imports: [MatButtonModule, MatDividerModule, MatIconModule]
 })
 export class TradeComponent {
+  orderDue: number = 69.69;
 
+  getOrderDue(): string {
+    return this.orderDue.toString();
+  }
 }
